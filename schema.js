294db7d062f2dc149eb71d05e2e18411cd43cc46
Atharva Joshi <atharvaj2160@gmail.com>
2022-01-08 22:39:36 +0530

    Added CRUD Operations

--- schema.js
+++ schema.js
@@ -6,24 +6,13 @@ const {
     GraphQLInt,
     GraphQLID,
     GraphQLSchema,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = graphql
+const Book = require("./models/books")
+const Author = require("./models/authors")
 
-// let books = [
-//     { name: "The Hard Thing About Hard Things",genre: "Fantasy", id: "1", authorID: "1" },
-//     { name: "The 10X Rule",genre: "Action", id: "2", authorID: "2"  },
-//     { name: "The Lean Startup",genre: "Mystery", id: "3", authorID: "3"  },
-//     { name: "Think Like a Monk",genre: "Mystery", id: "3", authorID: "2"  },
-//     { name: "Atomic Habits",genre: "Mystery", id: "3", authorID: "3"  },
-//     { name: "Tools of Titans",genre: "Mystery", id: "3", authorID: "1"  },
-// ]
-
-// let authors = [
-//     { name: "Tom Freslin",age: 44 , id: "1" },
-//     { name: "Grant Cardone",age: 33 , id: "2" },
-//     { name: "James Clear",age: 23 , id: "3" },
-// ]
-
+// DEFINING BOOK AND AUTHOR TYPES
 const BookType = new GraphQLObjectType({
     name: "Book",
     fields: () => ({
@@ -32,7 +21,7 @@ const BookType = new GraphQLObjectType({
         genre: { type: GraphQLString },
         author: {
             type: AuthorType,
-            resolve: (parent, args) => _.find(authors, {id: parent.authorID})
+            resolve: (parent, args) => Author.findById(parent.authorId)
         }
     })
 })
@@ -45,35 +34,74 @@ const AuthorType = new GraphQLObjectType({
         age: { type: GraphQLInt },
         books: {
             type: new GraphQLList(BookType),
-            resolve: (parent, args) => _.filter(books, { authorID: parent.id })
+            resolve: (parent, args) => Book.find({authorId: parent.id})
         }
     })
 })
-
+// TO GET DATA FROM MONGODB
 const rootQuery = new GraphQLObjectType({
     name: "RootQuery",
     fields: {
         book: {
             type: BookType,
             args: { id: { type: GraphQLID } },
-            resolve: (parent, args) => _.find(books, { id: args.id })
+            resolve: (parent, args) => Book.findById(args.id)
         },
         author: {
             type: AuthorType,
             args: { id: { type: GraphQLID } },
-            resolve: (parent, args) => _.find(authors, { id: args.id })
+            resolve: (parent, args) => Author.findById(args.id)
         },
         books: {
             type: new GraphQLList(BookType),
-            resolve: (parent, args) => books
+            resolve: (parent, args) => Book.find({})
         },
         authors: {
             type: new GraphQLList(AuthorType),
-            resolve: (parent, args) => authors
+            resolve: (parent, args) => Author.find({})
         }
     }
 })
 
+// TO PARSE DATA TO MONGO DB
+const Mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) }
+            },
+            resolve: (parent, args) => {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                })
+                return author.save()
+            }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                genre: { type: new GraphQLNonNull(GraphQLString) },
+                authorId: { type: new GraphQLNonNull(GraphQLID) }
+            },
+            resolve: (parent, args) => {
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                })
+                return book.save()
+            }
+        }
+    }
+})
+
+// EXPORT MODULES
 module.exports = new GraphQLSchema({
-    query: rootQuery
+    query: rootQuery,
+    mutation: Mutation
 })
